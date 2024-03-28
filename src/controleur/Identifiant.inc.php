5c4e3f44fa87ab49b383cb8500673a874a7b2389
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2024-03-28 15:21:26 +0100

    ajout lecture mdp

---
+++ src/controleur/Identifiant.inc.php
@@ -0,0 +1,18 @@
+<?php
+class Identifiant
+{
+	public static $user;
+	public static $mdp;
+
+	public function __construct()
+	{
+		$fichier = 'login.txt';
+		$ressource = fopen($fichier, 'rb');
+
+		self::$user = fgets($ressource);
+		self::$mdp = fgets($ressource);
+
+		fclose($ressource);
+	}
+}
+?>
