41694dd577452f4d9716691cc5b880c5c961b974
Amaxi76 <lesterlinpro@gmail.com>
2024-03-29 20:01:10 +0100

    Défilement des tableaux synchroniser

--- src/vue/visualiser/visualiser.js
+++ src/vue/visualiser/visualiser.js
@@ -1,30 +1,21 @@
-const table1 = document.querySelector('.tableau-nom-etd');
-const table2 = document.querySelector('.tableau-note-etd');
+const table1 = document.querySelector('.liste-etd');
+const table2 = document.querySelector('.liste-note');
 
-
-console.log(document.querySelector('.conteneur-visualiser'));
-
-// Ajoute un gestionnaire d'événement de défilement à la div de conteneur de défilement
-document.querySelector('.conteneur-visualiser').addEventListener('scroll', function() {
-    // Applique le défilement à chaque tableau
-    table1.scrollTop = this.scrollTop;
-    table2.scrollTop = this.scrollTop;
+table1.addEventListener('scroll', function() {
+	table2.scrollTop = table1.scrollTop;
 });
 
-function TableSetScroll(){
-	$('#headerTop').offset(
-		{
-			top:$('#conteneur-visualiser').offset().top
-		}
-	);
-	 
-	$('#headerLeft').offset(
-		{
-			top: $('#content').offset().top
-		}
-	);
-	 
-	 
-}
-TableSetScroll();
-$('.conteneur-visualiser').bind('scroll',TableSetScroll);
+table2.addEventListener('scroll', function() {
+	table1.scrollTop = table2.scrollTop;
+});
+
+// window.addEventListener('resize', function() {
+
+// 	const tableau1 = document.querySelector('tableau-nom-etd');
+// 	const tableau2 = document.querySelector('tableau-note-etd');
+
+// 	const headerHeight = tableau1.querySelector('th').offsetHeight;
+// 	tableau2.querySelector('thead').style.height = headerHeight + 'px';
+// });
+
+// function adaptationHauteur 
