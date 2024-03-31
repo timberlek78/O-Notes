8269938b1b02566ccf4e7843e746862fe5c9f4e2
Amaxi76 <lesterlinpro@gmail.com>
2024-03-31 10:33:16 +0200

    Ajout de la navigation entre les semestres

--- src/vue/visualiser/visualiser.js
+++ src/vue/visualiser/visualiser.js
@@ -1,30 +1,31 @@
 /*                        */
 /*   SCROLL SYNCHRONISE   */
 /*                        */
-const table1 = document.querySelector('.liste-etd');
-const table2 = document.querySelector('.liste-note');
+const table1 = document.querySelector ( '.liste-etd'  );
+const table2 = document.querySelector ( '.liste-note' );
 
 /* Permet de faire défiler les deux tableaux en même temps */
-table1.addEventListener('scroll', function() {
+table1.addEventListener ( 'scroll', function ( )
+{
 	table2.scrollTop = table1.scrollTop;
-});
+} );
 
-table2.addEventListener('scroll', function() {
+table2.addEventListener ( 'scroll', function ( )
+{
 	table1.scrollTop = table2.scrollTop;
-});
+} );
 
 
 /* Fonction qui permet d'adapter la hauteur de l'entête du tableau selon la hauteur maximal entre les deux tableaux */
-function adaptationHauteur () 
+function adaptationHauteur ( )
 {
+	const hauteurTableauNom  = document.querySelector ( '.tableau-nom-etd thead'  );
+	const hauteurTableauNote = document.querySelector ( '.tableau-note-etd thead' );
 
-const hauteurTableauNom  = document.querySelector('.tableau-nom-etd thead');
-const hauteurTableauNote = document.querySelector('.tableau-note-etd thead');
+	const hauteurMax = Math.max ( hauteurTableauNom.clientHeight, hauteurTableauNote.clientHeight );
 
-const hauteurMax = Math.max(hauteurTableauNom.clientHeight, hauteurTableauNote.clientHeight);
-
-hauteurTableauNom.style.height = hauteurMax + 'px';
-hauteurTableauNote.style.height = hauteurMax + 'px';
+	hauteurTableauNom .style.height = hauteurMax + 'px';
+	hauteurTableauNote.style.height = hauteurMax + 'px';
 }
 
 /* Déclanche la méthode lors du chargement de la page et du resize de la page */
@@ -77,3 +78,24 @@ function fermeturePopupEditionEtudiant ( )
 {
 	popupEditionEtudiant.classList.remove ( 'ouvert' );
 }
+
+/*                                     */
+/*   BAR DE NAVIGATION DES SEMSETRES   */
+/*                                     */
+
+const ensBoutonsSemestre = document.querySelectorAll ( '.btn-pied' );
+
+console.log (ensBoutonsSemestre)
+
+ensBoutonsSemestre.forEach ( function ( bouton )
+{
+	bouton.addEventListener ( 'click', function ( event )
+	{
+		ensBoutonsSemestre.forEach ( function ( bouton )
+		{
+			bouton.classList.remove ( 'btn-clique' );
+		} )
+		
+		bouton.classList.add ( 'btn-clique' );
+	} )
+} );
