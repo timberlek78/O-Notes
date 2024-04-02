e89093760e1bb1a638204951db85aa5bee711db7
Yanis Verdier <yverdier.pro@gmail.com>
2024-04-02 14:38:51 +0200

    Début du controleur vue

---
+++ src/controleur/ControleurVue.inc.php
@@ -0,0 +1,40 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	require "ControleurDB.inc.php";
+
+	class ControleurVue
+	{
+		private $DB;
+
+		public function __construct()
+		{
+			$this->DB = DB::getInstance();
+			$retour = $this->DB->selectAll("Etudiant");
+
+			$tabDonnees = array();
+			
+			foreach ($retour as $etudiant)
+			{
+				$etudiantDetails = array
+				(
+					'NIP' => $etudiant->getNIP(),
+					'nom' => $etudiant->getNom(),
+					'prenom' => $etudiant->getPrenom(),
+					'parcours' => $etudiant->getParcours(),
+					'promotion' => $etudiant->getPromotion()
+				);
+
+				$tabDonnees[] = $etudiantDetails;
+			}
+
+			$json = json_encode($tabDonnees);
+
+			echo "JASON : " . $json;
+		}
+	}
+
+	new ControleurVue();
+?>
