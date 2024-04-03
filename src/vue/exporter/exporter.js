2516e4bf09bd171ecf6c97c68e1111b1e560f503
Yanis Verdier <yverdier.pro@gmail.com>
2024-04-03 13:44:27 +0200

    Changement de l'exportation

--- src/vue/exporter/exporter.js
+++ src/vue/exporter/exporter.js
@@ -1,3 +1,19 @@
+/* CREATION DES ENCADRES */
+
+function fetchAnneeData(annee)
+{
+	fetch('../../controleur/ControleurVue.inc.php?annee=' + annee)
+		.then (reponse => reponse.json())
+		.then (donnees =>
+			{
+				console.log(donnees);
+			})
+		.catch (erreur => console.error('Erreur lors du fetch'))
+}
+
+
+/************************************/
+
 const popupExportation = document.getElementById("popup-avis-poursuite");
 const btnValider       = document.querySelector(".btn-valider");
 const btnAnnuler       = document.querySelector(".btn-annuler");
@@ -16,3 +32,14 @@ function fermerPopUp ( )
 	popupExportation.classList.remove('ouvert');
 }
 
+/*
+	<div class="cadre-export">
+		<div class="conteneur-image-etudiant">
+			<img class="photo-etudiant" alt="photo-etudiant" src="../../..../../../res/image/image.png">
+		</div>
+		<div class="conteneur-information">
+			<p>Prénom</p>
+			<p>Antunel-Celia_yanis</p>
+		</div>
+	</div>
+*/
