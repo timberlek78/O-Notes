582c3ee66dbaa4ba7e06994d57556bfea5d390d2
Amaxi76 <maximilien.lesterlin@etu.univ-lehavre.fr>
2024-04-04 21:36:16 +0200

    Séparation des différents élément de visualiser

--- src/vue/visualiser/visualiser.js
+++ src/vue/visualiser/visualiser.js
@@ -45,24 +45,3 @@ function adaptationHauteur ( )
 // Modication de la taille des en-tete
 const observer = new MutationObserver ( adaptationHauteur );
 observer.observe ( document.body, { attributes: true, childList: true, subtree: true } );
-
-/*                                                  */
-/*   FONCTIONNEMENT DE LA POP-UP EDITION ETUDIANT   */
-/*                                                  */
-const popupEditionEtudiant = document.getElementById ( 'popup-edition-etudiant'  );
-const btnEdition           = document.querySelector  ( '.edition'                );
-const btnAnnuler           = document.querySelector  ( '.btn-annuler'            );
-const btnValider           = document.querySelector  ( '.btn-valider'            );  //TODO: validation
-
-btnEdition.addEventListener ( 'click', ouverturePopupEditionEtudiant );
-btnAnnuler.addEventListener ( 'click', fermeturePopupEditionEtudiant );
-
-function ouverturePopupEditionEtudiant ( )
-{
-	popupEditionEtudiant.classList.add ('ouvert');
-}
-
-function fermeturePopupEditionEtudiant ( )
-{
-	popupEditionEtudiant.classList.remove ( 'ouvert' );
-}
