0ce8a154efbabb96882e0b1e1e4a6bb2d74c5d63
Amaxi76 <lesterlinpro@gmail.com>
2024-04-05 10:28:15 +0200

    Ombre + Session + et d'autres trucs

--- src/metier/authentification/gestionLogin.php
+++ src/metier/authentification/gestionLogin.php
@@ -30,7 +30,6 @@
 
 	function verification ( $nom, $pass ) : Utilisateur
 	{
-		return new Utilisateur ( "fds", "dsfs", "dsfs" );
 		$lstUtilisateur =  DB::getInstance ( )->selectAll ( "utilisateur" );
 
 		foreach ( $lstUtilisateur as $utilisateur )
